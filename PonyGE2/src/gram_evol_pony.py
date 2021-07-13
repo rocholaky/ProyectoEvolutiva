6602f63888707231b5f3e5cf07f2a3b7349b41f2
rocholaky <roberto.cholaky@ug.uchile.cl>
2021-07-13 10:49:54 -0400

    adding neuronal_individual

---
+++ PonyGE2/src/gram_evol_pony.py
@@ -0,0 +1,24 @@
+import utils.evolutionary_EQL
+import utils.function_class_eql
+import torch
+from utils.network_parts import power_Module
+import numpy as np
+import re
+
+class network_builder_pony:
+    def __init__(self, string, init_variables=2):
+        self.n_variables = init_variables
+        # string to list
+        decoded_grammar = []
+        list_string = re.findall('(\d+|[A-Za-z]+)', string) # ejemplo: '12blockPow2' --> ['12', 'blockPow', '2', '1']
+        for i in range(len(list_string)): # ejemplo: ['12', 'blockPow', '2'] --> [1, 2, 'blockPow', 2, 1]
+            if list_string[i].isdigit():
+                for j in list(list_string[i]):
+                    decoded_grammar.append(int(j))
+            else:
+                decoded_grammar.append(list_string[i])
+        # Dividir lista en sublista de 4 (cada sublista 1 capa)
+        # ejemplo: [1, 2, 'blockPow', 2, 1] --> [[1,2,'blockPow',2], [1]]
+        decoded_grammar = [decoded_grammar[i:i+4] for i in range(0, len(decoded_grammar), 4)]
+        # TODO: Intentar hacer calzar con el codigo de antes del proyecto
+        for layer in decoded_grammar:
---
+++ PonyGE2/src/neuronal_individual.py
@@ -0,0 +1,56 @@
+import sys
+from collections import deque
+from representation.individual import Individual
+sys.path.append("C:\\Users\\rocho\\OneDrive\\Documentos\\Universidad\\Computacion_evolutiva\\ProyectoEvolutiva\\utils")
+import gram_evol
+from algorithm.parameters import params
+from algorithm.mapper import mapper
+
+class EQL_individual(Individual):
+    '''
+    GEQl
+    A Ge EQL individual
+    '''
+
+    def __init__(self, genome, ind_der, map_ind=True):
+        """
+        Initialise an instance of the individual class (i.e. create a new
+        individual).
+
+        :param genome: An individual's genome.
+        :param ind_der: An individual's derivation to EQL network 
+        :param map_ind: A boolean flag that indicates whether or not an
+        individual needs to be mapped.
+        """
+
+        if map_ind:
+            self.phenotype, self.genome ,self.EQL_nn, self.invalid = mapper(genome, ind_der)
+
+        else:
+            self.genome, self.der = genome, ind_der
+
+        self.fitness = params["FITNESS_FUNCTION"].default_fitness
+        self.runtime_error = False
+        self.name = None
+
+
+'''
+EQL network mapper function used for converting a genome to phenome.
+'''
+def mapper(genome):
+    assert genome
+    genome = list(genome) # making a copy of the indiivdual
+    max_wraps = params['MAX_WRAPS']
+    # we return a network from a genome: 
+    bnf_grammar = params['BNF_GRAMMAR']
+    output = deque()
+    unexpanded_symbol = deque([(bnf_grammar, 1)])
+    wraps = -1
+    while (wraps< max_wraps) and unexpanded_symbol:
+        if wraps > max_wraps:
+            break 
+        
+    return phenotype, gen, network
+
+
+
