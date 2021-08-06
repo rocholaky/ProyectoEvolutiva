48947227127402882fcb3bf201229c2a361008a1
rocholaky <roberto.cholaky@ug.uchile.cl>
2021-08-06 10:56:56 -0400

    RC: solucion regularizacion en evaluacion

--- PonyGE2/src/fitness/supervised_learning/EqlFitness.py
+++ PonyGE2/src/fitness/supervised_learning/EqlFitness.py
@@ -3,6 +3,8 @@ import numpy as np
 from utilities.fitness.get_data import get_data
 from algorithm.parameters import params
 from utilities.fitness.error_metric import mse
+from torch import nn
+import torch
 
 class EqlFitness(base_ff):
     def __init__(self):
@@ -14,6 +16,7 @@ class EqlFitness(base_ff):
         self.training_exp = self.training_exp[:, np.newaxis].astype('float32')
         self.test_in = self.test_in.astype('float32').transpose().astype('float32')
         self.test_exp = self.test_exp[:, np.newaxis].astype('float32')
+        self.L1_reg = nn.L1Loss(reduction='sum')
             
 
     # Mejor usar el mse que viene en el archivo error_metrics.py
@@ -22,4 +25,9 @@ class EqlFitness(base_ff):
     #    return np.mean(np.square(self.training_exp - pred))
 
     def evaluate(self, ind, **kwargs):
-        return mse(self.test_exp, ind(self.test_in).cpu().detach().numpy())
+        lamda = params['reg_pon']
+        param_loss = 0
+        with torch.no_grad():
+            for parameter in ind.phenotype.parameters():
+                param_loss += lamda*self.L1_reg(parameter, torch.zeros_like(parameter)).cpu().detach().numpy()
+        return mse(self.test_exp, ind(self.test_in).cpu().detach().numpy()) + param_loss
