ebe730b87a8c022ffdc735f57a317861093b9301
rocholaky <roberto.cholaky@ug.uchile.cl>
2021-08-12 11:02:25 -0400

    RC: datasets de Cdrag_FB y Nu_FB añadidos

--- PonyGE2/datasets/dataset_util.py
+++ PonyGE2/datasets/dataset_util.py
@@ -1,7 +1,12 @@
+from torch.utils import data
 from torch.utils.data import Dataset
 import numpy as np
 import matplotlib.pyplot as plt
 from tqdm.auto import tqdm
+from os import path, mkdir, getcwd
+from inspect import getsource
+from math import ceil
+import os
 
 
 # Dataset object that creates a artificial dataset given the number of points, the number of variables and
@@ -40,14 +45,43 @@ class AutoGenerating_dataset(Dataset):
         return x_values, y_values
 
 
+    def save_file(self,folder_name):
+        ######
+        # create and save new dataset
+        script_dir, _ = os.path.split(os.path.abspath(__file__))
+        dataset_path = path.join(script_dir, folder_name)
+        mkdir(dataset_path)
+        y_dataset = self.function(self.x_dataset)[..., np.newaxis]
+        train_Xy = np.concatenate([self.x_dataset, y_dataset], -1)
+        np.savetxt(path.join(dataset_path, "Train.txt"), train_Xy, delimiter='\t', fmt='%.11f')
+        test_points = ceil(0.3*self.n_points)
+        test_data = np.hstack([np.random.uniform(bound[0], bound[1], (self.n_points, 1)) for bound in self.bound_list])
+        y_test = self.function(test_data)[..., np.newaxis]
+        test_Xy = np.concatenate([test_data, y_test], -1)
+        np.savetxt(path.join(dataset_path, "Test.txt"), test_Xy, delimiter='\t', fmt='%.11f')
+
+        # save parameters info
+        filename = path.join(dataset_path, "dataset_info.txt")
+        savefile = open(filename, 'w')
+        savefile.write(getsource(function) + '\n')
+        savefile.write('n_variables: ' + str(n_variables) + '\n')
+        savefile.write('------------------\n')
+        savefile.write('input_range: ' + str(self.bound_list) + '\n')
+        savefile.write('n_points_train: ' + str(self.n_points) + '\n')
+        savefile.write('n_points_test: ' + str(test_points) + '\n')
+        savefile.close()
+
+
 
 if __name__ == '__main__':
     n_points = 1000
     n_variables = 2
     # function we want to guess:
-    function = lambda x: np.square(x[:,0]) + x[:, 1] + np.random.random(x[:, 0].shape)
-    bound_list = [[-1, 1], # x values
-                  [-2, 2]] # y values
+    function = lambda x: 2.0232*np.power(x[:, 0], 0.5528)
+    bound_list = [[1.3, 2.4], # x values
+                  [1000, 11000],
+                  [0.3, 1.5],
+                  [0.6, 1.05]] # y values
 
     dataset = AutoGenerating_dataset(n_points, n_variables, function, bound_list)
     item = dataset.__getitem__([0, 1])
@@ -58,4 +92,5 @@ if __name__ == '__main__':
     axs[0].plot(1,1,dataset.x_dataset[0:200,0])
     axs[1].plot(2,1,dataset.x_dataset[0:200,1])
     plt.show()
+    dataset.save_file("Nu_FB")
 
